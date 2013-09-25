5de26893de5e49281b54c74ac472349de87965c3
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-09-25 17:38:59 -0500

    testing travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 FinalProject::Application.routes.draw do
   
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login',  to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 end
