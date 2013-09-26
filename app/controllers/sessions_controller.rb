e296edf355526b0f84d6a15b6cbd5b79ca6f06ba
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-09-25 19:31:24 -0500

    created login/logout with tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,27 @@
 class SessionsController < ApplicationController
 
 	def create
+  	@user = User.find_by_username(params[:session][:username])
+    if @user == nil
+      flash[:error] = "The user you have entered does not exist"
+      redirect_to login_path
+    else
+      if @user.is_active?
+        if  @user.authenticate(params[:session][:password])
+          session[:user_id] = @user.id
+          redirect_to '/', :notice => "Signed in!"
+        else
+          flash[:error] = "Username or Password is incorrect"
+          redirect_to login_path, :notice => "Signed in!"
+        end
+      else
+        @error = "Your account is not active"
+      end
+    end
+  end
 
-	end
-
-	def destroy
- 
+  def destroy
+    session.clear
+    redirect_to('/')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 FinalProject::Application.routes.draw do
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login',  to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+
 end
