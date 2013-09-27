818b4bfb0bb163295a39ed158d02b684cb5fdadf
Luis Hernandez <lherna1371@gmail.com>
2013-09-27 18:48:48 -0500

    hope this works

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,4 @@
 Paperclip.interpolates(:s3_sg_url) do |att, style| 
-"#{att.s3_protocol}://s3-ap-southeast-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+"#{att.s3_protocol}://#{att.bucket_name}.s3.amazonaws.com//#{att.path(style)}"
 end
+
