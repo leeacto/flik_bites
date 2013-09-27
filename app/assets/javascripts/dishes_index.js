fd1718b322115977221466a2b400e29e3d6dca17
Nicholas <nicholas.lee.3@gmail.com>
2013-09-27 15:45:29 -0500

    added js to tabs

--- app/assets/javascripts/dishes_index.js
+++ app/assets/javascripts/dishes_index.js
@@ -1,5 +1,34 @@
+function showStarterTab(){
+	$('.starters').removeClass('hidden');
+	$('.entrees').addClass('hidden');
+	$('.desserts').addClass('hidden');
+}
+
+function showEntreeTab(){
+	$('.starters').addClass('hidden');
+	$('.entrees').removeClass('hidden');
+	$('.desserts').addClass('hidden');
+}
+
+function showDessertTab(){
+	$('.starters').addClass('hidden');
+	$('.entrees').addClass('hidden');
+	$('.desserts').removeClass('hidden');
+}
+
 $(document).ready(function(){
+	$('#starters_tab').on('click', function(event){
+		event.stopPropagation();
+		showStarterTab();
+	});
 
+	$('#entrees_tab').on('click', function(event){
+		event.stopPropagation();
+		showEntreeTab();
+	});
 
-
+	$('#desserts_tab').on('click', function(event){
+		event.stopPropagation();
+		showDessertTab();
+	});
 });
