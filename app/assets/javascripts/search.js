4e61b239a45b3fe45d66683b47c6c906ebbb1f3b
Brian Tong <btong34@gmail.com>
2013-09-27 15:36:46 -0500

    add basic search functionality

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,13 @@
+var liveSearch = function() {
+    $("#search_field").bind("keyup", function() {
+        var url = $("#search_form").attr("action")
+        var data = $("#search_form").serialize();
+
+        $.get(url, data, function(response){
+            $("#live-search").html(response);
+        });
+    });
+};
+
+$(document).ready(liveSearch);
+$(document).on('page:load', liveSearch);
