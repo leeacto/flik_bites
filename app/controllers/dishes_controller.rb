22fe589ac48a66441f64194745097e6d70e4cde8
Luis Hernandez <lherna1371@gmail.com>
2013-09-28 14:39:17 -0500

    photo controller work

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -21,7 +21,7 @@ class DishesController < ApplicationController
     for_url = params[:dishname].gsub(" ", "").downcase
     @dish = Dish.where(:url => for_url).first
     @photo = Photo.new
-    @photo_view = Photo.last
+    @photos = Photo.where(:dish_id => @dish.id)
 
     if @dish.nil?
       render 'not_found'
