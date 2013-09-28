b104897ffb958dd17b4366b6bbb1ba7d899d9167
Luis Hernandez <lherna1371@gmail.com>
2013-09-28 11:43:24 -0500

    pic flip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,15 +2,9 @@ class Photo < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :dish
 	
-  has_attached_file :image, {
-        :styles => {
-          :medium => ["654x500>", :jpg],
-          :thumb =>["200x200#", :jpg]
-        },
-        :convert_options => {
-          :medium => "-quality 80 -interlace Plane",
-          :thumb => "-quality 80 -interlace Plane"
-          }
-        },
-         :processors => [:thumbnail, :compression]
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }, :processors => [:thumbnail, :compression]
 end
