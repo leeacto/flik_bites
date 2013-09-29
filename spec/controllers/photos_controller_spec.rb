892d0e1d4949d6ff1f7e485518e74c63aeef6359
Luis Hernandez <lherna1371@gmail.com>
2013-09-29 14:19:51 -0500

    css work to restaurant view and pust to github

--- spec/controllers/photos_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-describe PhotosController do
-	it "should be able to attach a file" do
-		@user = FactoryGirl.create(:user)
-		post :update,{format: "#{@user.id}"}
-		@user.reload.is_active?.should eq false
-		session[:user_id].should eq nil
-		response.should redirect_to(root_path)	
-	end
-end
