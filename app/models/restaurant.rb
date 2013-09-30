328efd4a7cd8edf931e2ccc75b62d65c7d526c36
Nicholas <nicholas.lee.3@gmail.com>
2013-09-30 18:39:33 -0500

    before merge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :dishes
-	
+	has_many :photos, through: :dishes
 	validates_uniqueness_of :name, scope: :address
 	validates_presence_of :name
 	validates_presence_of :address
