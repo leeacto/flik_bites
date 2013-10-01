c2ab46003a0c1fa32a9891f785d6f22358b3441f
Brian Tong <btong34@gmail.com>
2013-10-01 15:19:23 -0500

    add redirect_to_back helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,4 +47,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def redirect_to_back(default = root_url)
+    if !request.env["HTTP_REFERER"].blank? and request.env["HTTP_REFERER"] != request.env["REQUEST_URI"]
+      redirect_to :back
+    else
+      redirect_to default
+    end
+  end
+  
 end
