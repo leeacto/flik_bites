9f7379964a183df9e320dad7339faefbd5791436
Luis Hernandez <lherna1371@gmail.com>
2013-10-01 19:44:23 -0500

    completed work to comment controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -25,12 +25,12 @@ class DishesController < ApplicationController
   def show
     for_url = params[:dishname].gsub(" ", "").downcase
     @dish = Dish.find_by(:url => for_url)
-    @comment = Comment.where(:dish_id => @dish.id)
     if @dish.nil?
       render 'not_found'
     else
       @photo = Photo.new
       @photos = Photo.where(:dish_id => @dish.id)
+      @comment = Comment.where(:dish_id => @dish.id)
     end
   end
 
