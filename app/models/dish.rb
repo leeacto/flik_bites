4f8469725ffa8abef8c8dfccf53d24728835a358
Brian Tong <btong34@gmail.com>
2013-10-02 10:03:20 -0500

    remove scoping for predefined categories

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,7 +5,5 @@ class Dish < ActiveRecord::Base
   validates_presence_of :name, :category
   validates_uniqueness_of :restaurant_id, scope: :name
   
-  scope :starters, -> { where(category: 'Starter') }
-	scope :entrees, -> { where(category: 'Entree') }
-	scope :desserts, -> { where(category: 'Dessert') }
+
 end
