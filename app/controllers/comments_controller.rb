4e9188ec77e317886e51a79b382ce46c5528c52a
Luis Hernandez <lherna1371@gmail.com>
2013-10-01 19:46:25 -0500

    completed work to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,8 @@ class CommentsController < ApplicationController
 
 
 	def create
-		# puts "************************************************************"
-		# puts params[:comments][:content]
-		# puts params[:comments][:rating]
-		# puts params[:comments][:dish_id]
-		# puts params
 		comment = Comment.create(user_id: current_user.id, dish_id: params[:comments][:dish_id], content: params[:comments][:content], rating: params[:comments][:rating])
 		redirect_to "/"
-
-		# redirect_to "/#{@restaurant.url}/#{@dish.url}"
 	end 
 
 end
