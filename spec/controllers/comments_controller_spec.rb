fe12b67d682deb5830cbea5a331fb907ecbc9c38
Luis Hernandez <lherna1371@gmail.com>
2013-10-02 18:12:42 -0500

    implementation of comment form and tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,24 +1,24 @@
-# require 'spec_helper'
-# include UserHelper
+require 'spec_helper'
+include UserHelper
 
 
-# describe CommentsController do
+describe CommentsController do
 
-# 	describe Comment do
-# 	    describe "Create comment" do
-# 	    	it "should create a new comment" do
-# 	      		expect { Comment.create(content: "good stuff", 
-# 	        		rating: 1, 
-# 	         		user_id: 1, 
-# 	         		dish_id: 1) }.to change(Comment, :count).by(1)
-# 	      	end 
+	describe Comment do
+	    describe "Create comment" do
+	    	it "should create a new comment" do
+	      		expect { Comment.create(content: "good stuff", 
+	        		rating: 1, 
+	         		user_id: 1, 
+	         		dish_id: 1) }.to change(Comment, :count).by(1)
+	      	end 
 
-# 	      	it "should not create comment, input not valid" do
-# 	        	expect { Comment.create(content: " ", 
-# 	        		rating: 1, 
-# 	         		user_id: 1, 
-# 	         		dish_id: 1) }.to_not change{Comment.count}
-# 	      	end
-# 	    end
-# 	end 
-# end
+	      	it "should not create comment, input not valid" do
+	        	expect { Comment.create(content: " ", 
+	        		rating: 1, 
+	         		user_id: 1, 
+	         		dish_id: 1) }.to_not change{Comment.count}
+	      	end
+	    end
+	end 
+end
