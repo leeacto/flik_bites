260ceaa294ae137d5133469b0ebe743c52e980f0
Nicholas <nicholas.lee.3@gmail.com>
2013-10-01 19:19:19 -0500

    flip card changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ FinalProject::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do not fallback to assets pipeline if a precompiled asset is missed. MAY HAVE TO CHANGE BACK TO FALSE
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
