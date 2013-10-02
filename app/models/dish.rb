7d4ab5feb3af36c77ec27c9004f760c1cdde15c4
Brian Tong <btong34@gmail.com>
2013-10-02 10:06:06 -0500

    add search method to Dish model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,5 +5,9 @@ class Dish < ActiveRecord::Base
   validates_presence_of :name, :category
   validates_uniqueness_of :restaurant_id, scope: :name
   
-
+  def self.search(term)
+    if term
+      where('lower(name) LIKE ?', "%#{term.downcase.strip}%")
+    end
+  end
 end
