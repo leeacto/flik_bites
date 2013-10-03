c63c1f30956bcd20ee18eef39b27bf0e2ae78eea
Eugene Filimonov <eugenefilimonov@gmail.com>
2013-10-03 07:13:43 -0500

    fixing some more css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 FinalProject::Application.routes.draw do
   get '/uploads/:dish_id', to: 'dishes#upload'
   post '/favorite/dish/:dish_id', to: 'users#favdish', as: "favorite"
-  get '/favorite/restaurant/:rest_id', to: 'users#favrest'
+  post '/favorite/restaurant/:rest_id', to: 'users#favrest'
   get '/users/:user_id/favorites', to: 'users#favorites'
   match 'auth/:provider/callback', to: 'sessions#createoauth', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
