6414b1d6ec42492b1cf99aedae2c121ffc5c4ce8
Brian Tong <btong34@gmail.com>
2013-10-03 12:51:19 -0500

    uncoment compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ FinalProject::Application.configure do
 
   config.assets.compress = true
 
-  # config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed. 
   config.assets.compile = false
